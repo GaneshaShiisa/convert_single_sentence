2c04849d6132c874e501d0ec633e2c5b9f4f8b10
GaneshaShiisa <Ganesha.Shiisa@gmail.com>
2017-05-27 21:45:15 +0900

    new

--- convert_single_sentence.py
+++ convert_single_sentence.py
@@ -1,1 +1,26 @@
-print('HELLO')
+#
+#
+import sys
+# 引数確認
+argvs = sys.argv
+argc = len(argvs)
+if argc != 2:
+    print('Usage: # python %s filename', argvs[0])
+    quit()
+filename = argvs[1]
+f = open(filename, 'r')
+data = f.read()
+f.close()
+print(data)
+data = data.replace('# ', '')
+data = data.replace('\n', ' ')
+f = open('test.txt', 'w')
+while len(data) > 0:
+    priod_index = data.find('.')
+    # : !でも改行したい
+    sentence = data[0: priod_index + 1]
+    f.writelines(sentence + '\n')
+    print(sentence)
+    data = data[priod_index + 2:]
+print('end')
+f.close()
